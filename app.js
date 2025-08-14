0d5a1d2e05cf8332eed176f17221e35a529e21db
kunaljha17 <jhakunal124@gmail.com>
2025-08-15 00:36:26 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let inp = document.querySelector('input');
+let btn = document.querySelector('button');
+let ul = document.querySelector('ul');
+
+btn.addEventListener("click",function(){
+    let item = document.createElement('li');
+    item.innerText = inp.value;
+    let delbtn = document.createElement('button');
+    delbtn.innerText = "delete";
+    delbtn.classList.add("delete");
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    inp.value= "";
+})
+
+ul.addEventListener("click",function(event){
+    
+    if(event.target.nodeName=="BUTTON"){
+        let listitem = event.target.parentElement;
+        listitem.remove();
+        console.log("deleted succesfully");
+
+    }
+
+});
+
